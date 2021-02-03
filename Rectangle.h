4832de0c2a6b6016b69f950d32fbfdc2d597c832
Julian Krauth <mail@j-krauth.de>
2021-02-03 13:21:40 +0100

    added private parts

--- Rectangle.h
+++ Rectangle.h
@@ -1,12 +1,16 @@
 /* File : Rectangle.h */
 
 class Rectangle {
-    public:
-        int x0, y0, x1, y1;
-        Rectangle();
-        Rectangle(int x0, int y0, int x1, int y1);
-        ~Rectangle();
-        int getArea();
-        void getSize(int* width, int* height);
-        void move(int dx, int dy);
+private:
+  int x0 = 0;
+  int y0 = 0;
+  int x1 = 1;
+  int y1 = 1;  
+public:
+  Rectangle();
+  Rectangle(int x0, int y0, int x1, int y1);
+  ~Rectangle();
+  int getArea();
+  void getSize(int* width, int* height);
+  void move(int dx, int dy);
 };
